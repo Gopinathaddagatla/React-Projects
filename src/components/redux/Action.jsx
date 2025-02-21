ccf621296fc7d4c4da246b0d7e0f6bbe80d152c2
Your Name <you@example.com>
2025-02-21 22:16:05 +0530

    action state modified

--- src/components/redux/Action.jsx
+++ src/components/redux/Action.jsx
@@ -1,7 +1,3 @@
-export const ADD_TO_CART = 'ADD_TO_CART';
-export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
-export const SET_CART = 'SET_CART';
-
 export const addToCart = (product) => ({
     type: "ADD_TO_CART",
     payload: product,
